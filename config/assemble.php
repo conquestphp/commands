683e0ae9e78b705bb4732b7b42f12ef45946932b
jdw5 <joshuadwallace1@gmail.com>
2024-08-08 11:54:25 +1000

    Templating out new commands and stubs

--- config/assemble.php
+++ config/assemble.php
@@ -4,9 +4,9 @@
 return [
     'extension' => 'vue',
     'paths' => [
-        // 'page' => resource_path('js/Pages'),
-        // 'modal' => resource_path('js/Modals'),
-        // 'lib' => resource_path('js/Lib),
-        // 'component' => resource_path('js/Components'),
+        'page' => resource_path('js/Pages'),
+        'modal' => resource_path('js/Modals'),
+        'lib' => resource_path('js/Lib'),
+        'component' => resource_path('js/Components'),
     ]
 ];

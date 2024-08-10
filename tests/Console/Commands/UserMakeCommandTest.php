2cd43ab6974377a637b29b9b75992e2f1c96fc5c
jdw5 <joshuadwallace1@gmail.com>
2024-08-10 14:39:39 +1000

    Abstract user role command

--- tests/Console/Commands/UserMakeCommandTest.php
+++ tests/Console/Commands/UserMakeCommandTest.php
@@ -4,11 +4,23 @@ use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Artisan;
 
 it('can create a user', function () {
-    Artisan::call('user:make', [
+    $result = Artisan::call('user:make', [
         'name' => 'John Doe',
         'email' => 'john@example.com',
         'password' => 'password123',
     ]);
     
     expect(User::where('email', 'john@example.com')->exists())->toBeTrue();
+    expect($result)->toBe(1); // Successful
+});
+
+it('validates email', function () {
+    $result = Artisan::call('user:make', [
+        'name' => 'John Doe',
+        'email' => 'johnexample.com',
+        'password' => 'password123',
+    ]);
+    
+    expect(User::where('email', 'johnexample.com')->exists())->toBeFalse();
+    expect($result)->toBe(0); // Fail
 });

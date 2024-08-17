5237d33ea5baf3919dcf9296e2750931b5a3cc30
jdw5 <joshuadwallace1@gmail.com>
2024-08-17 21:39:01 +1000

    Rules

--- src/Enums/SchemaColumn.php
+++ src/Enums/SchemaColumn.php
@@ -17,8 +17,8 @@ enum SchemaColumn: string
     case Code = 'code';
     case Path = 'path';
     case ForeignId = '*_id';
-    case StartAt = 'starts_at';
-    case EndAt = 'ends_at';
+    case StartsAt = 'starts_at';
+    case EndsAt = 'ends_at';
     case IsBoolean = 'is_*';
     case Details = 'details';
     case Data = 'data';
@@ -40,6 +40,11 @@ enum SchemaColumn: string
     case Longitude = 'longitude';
     case Version = 'version';
     case Image = 'image';
+    case Url = 'url';
+    case CreatedBy = 'created_by';
+    case UpdatedBy = 'updated_by';
+    case IpAddress = 'ip_address';
+    case Icon = 'icon';   
     
     /** Coalseced for consistency */
     case File = 'file';
@@ -48,6 +53,8 @@ enum SchemaColumn: string
     case Priority = 'priority';
     case Lat = 'lat';
     case Long = 'long';
+    case Colour = 'colour';
+
 
     /** Undefined */
     case Undefined = 'undefined';
@@ -57,6 +64,11 @@ enum SchemaColumn: string
         return match ($this) {
             self::File => 'file',
             self::Title => 'title',
+            self::Notes => 'details',
+            self::Priority => 'order',
+            self::Lat => 'latitude',
+            self::Long => 'longitude',
+            self::Colour => 'color',
             default => null,
         };
     }
@@ -65,41 +77,46 @@ enum SchemaColumn: string
     {
         return match ($this) {
             self::Name, self::Title, => '$table->string(\'name\');',
-            self::Email => '$table->string(\'email\');',
+            // self::Email => '$table->string(\'email\');',
             self::Slug => '$table->string(\'slug\')->unique();',
-            self::Password => '$table->string(\'password\');',
-            self::Username => '$table->string(\'username\');',
-            self::FirstName => '$table->string(\'first_name\');',
-            self::LastName => '$table->string(\'last_name\');',
-            self::Description => sprintf('$table->text(\'description\', %s)->nullable();', $this->length()),
+            // self::Password => '$table->string(\'password\');',
+            // self::Username => '$table->string(\'username\');',
+            // self::FirstName => '$table->string(\'first_name\');',
+            // self::LastName => '$table->string(\'last_name\');',
+            self::Description => sprintf('$table->text(\'description\', %s)->nullable();', $this->length()()),
             self::Status => '$table->unsignedTinyInteger(\'status\')->default(0);',
-            self::Color => '$table->string(\'color\')->default(\'#000000\');',
-            self::Code => '$table->string(\'code\');',
+            self::Color, self::Colour => '$table->string(\'color\')->default(\'#000000\');',
+            // self::Code => '$table->string(\'code\');',
             self::Path, self::File => '$table->string(\'path\');',
             self::ForeignId => sprintf('$table->foreignId(\'%s\')->constrained()->onDelete(\'cascade\');', $value),
-            self::StartAt => '$table->dateTime(\'starts_at\');',
-            self::EndAt => '$table->dateTime(\'ends_at\')->nullable();',
+            self::StartsAt => '$table->dateTime(\'starts_at\');',
+            self::EndsAt => '$table->dateTime(\'ends_at\')->nullable();',
             self::IsBoolean => sprintf('$table->boolean(\'%s\')->default(false);', $value),
             self::Details, self::Notes => '$table->text(\'details\')->nullable();',
             self::Data => '$table->json(\'data\')->nullable();',
             self::Phone => '$table->string(\'phone\');',
             self::Price => '$table->unsignedInteger(\'price\')',
             self::Quantity => '$table->integer(\'quantity\')',
-            self::Address => '$table->string(\'address\');',
-            self::City => '$table->string(\'city\');',
-            self::State => '$table->string(\'state\');',
-            self::Zip => '$table->string(\'zip\');',
+            // self::Address => '$table->string(\'address\');',
+            // self::City => '$table->string(\'city\');',
+            // self::State => '$table->string(\'state\');',
+            // self::Zip => '$table->string(\'zip\');',
             self::Country => '$table->string(\'country\');',
             self::Duration => '$table->unsignedInteger(\'duration\');',
             self::Minutes => '$table->unsignedInteger(\'minutes\');',
             self::Uuid => '$table->uuid(\'uuid\');',
             self::Order, self::Priority => '$table->unsignedSmallInteger(\'order\');',
-            self::Token => '$table->string(\'token\')',
+            // self::Token => '$table->string(\'token\')',
             self::Coordinate => '$table->point(\'coordinate\')',
             self::Latitude, self::Lat => '$table->decimal(\'latitude\', 10, 8)',
             self::Longitude, self::Long => '$table->decimal(\'longitude\', 11, 8)',
             self::Version => '$table->unsignedSmallInteger(\'version\')',
-            self::Image => '$table->string(\'image\')',
+            self::CreatedBy => '$table->foreignId(\'created_by\')->nullable()->constrained(\'users\')->onDelete(\'cascade\');',
+            self::UpdatedBy => '$table->foreignId(\'updated_by\')->nullable()->constrained(\'users\')->onDelete(\'cascade\');',
+            // self::Image => '$table->string(\'image\')',
+            // self::Url => '$table->string(\'url\')',
+            // self::IpAddress => '$table->string(\'ip_address\')',
+            // self::Icon => '$table->string(\'icon\')',
             default => sprintf('$table->string(\'%s\');', $value),
         };
     }
@@ -116,7 +133,7 @@ enum SchemaColumn: string
             self::Username,
             self::FirstName,
             self::LastName,
-            self::Color,
+            self::Color, self::Colour,
             self::Code,
             self::Path, self::File,
             self::Phone, 
@@ -125,7 +142,8 @@ enum SchemaColumn: string
             self::State, 
             self::Zip, 
             self::Country,
-            self::Image => 255,
+            self::Image,
+            self::Icon, self::IpAddress => 255,
 
             self::Description => 512,
 
@@ -142,6 +160,60 @@ enum SchemaColumn: string
             self::Duration,
             self::Minutes => 4294967295, 
             default => 0,
+        };
+    }
+
+    public function rules(mixed $value, ?string $table = null): array
+    {
+        return match ($this) {
+            self::Email => ['required', 'string', 'email', sprintf('max:%s', $this->length())],
+            self::Slug => ['required', 'string', 'lowercase', sprintf('unique:%s,slug', $table), sprintf('max:%s', $this->length())],
+            self::Password => ['required', 'string', 'min:8', sprintf('max:%s', $this->length())],
+            self::Description => ['nullable', 'string', sprintf('max:%s', $this->length())],
+            self::Status => ['required', 'integer', 'min:0', sprintf('max:%s', $this->length())],
+            self::Color, self::Colour => ['nullable', 'hex_color', sprintf('max:%s', $this->length())],
+            self::ForeignId => ['required', 'integer', 'exists:users,id'],
+            self::StartsAt => ['required', 'date', 'after_or_equal:now'],
+            self::EndsAt => ['nullable', 'date', 'after_or_equal:now'],
+            self::IsBoolean => ['required', 'boolean'],
+            self::Data => ['required', 'json', sprintf('max:%s', $this->length())],
+            self::Price => ['required', 'integer', 'min:0', sprintf('max:%s', $this->length())],
+            self::Quantity => ['required', 'integer', 'min:0', sprintf('max:%s', $this->length())],
+            self::Duration => ['required', 'integer', 'min:0', sprintf('max:%s', $this->length())],
+            self::Minutes => ['required', 'integer', 'min:0', sprintf('max:%s', $this->length())],
+            self::Uuid => ['required', 'uuid'],
+            self::Order, self::Priority => ['required', 'integer', 'min:0', sprintf('max:%s', $this->length())],
+            self::Token => ['required', 'alpha_num', sprintf('max:%s', $this->length())],
+            self::Coordinate => ['required', 'point'],
+            self::Latitude => ['required', 'decimal', 'min:-90', 'max:90'],
+            self::Longitude => ['required', 'decimal', 'min:-180', 'max:180'],
+            self::Version => ['required', 'integer', 'min:0', sprintf('max:%s', $this->length())],
+            self::IpAddress => ['required', 'ip'],
+            self::Undefined => ['required'],
+            self::Url => ['required', 'url', sprintf('max:%s', $this->length())],
+            default => ['required', 'string', sprintf('max:%s', $this->length())],
+        };
+    }
+
+    public function cast(): ?string
+    {
+        return match ($this) {
+            self::StartsAt, self::EndsAt => '\'datetime\'',
+            self::Data => '\'json\'',
+            self::Price => 'Money::class',
+            default => null,
+        };
+    }
+
+    public function hidden(): bool
+    {
+        return match ($this) {
+            self::ForeignId, 
+            self::CreatedBy, 
+            self::UpdatedBy,
+            self::Password,
+            self::Token => true,
+            default => false,
         };
     }
 }
